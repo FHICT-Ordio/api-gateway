7a757f8ab06d13116826a82407f37db0e10f6eb1
Robin van Hoof <robbie@vanhoof-erp.nl>
2022-04-11 15:31:53 +0200

    Initial setup

---
+++ Ocelot API Gateway/Ocelot API Gateway/Program.cs
@@ -0,0 +1,41 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Ocelot_API_Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                        .AddJsonFile("ocelot.json")
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureServices(s =>
+                {
+                    s.AddOcelot();
+                })
+                .ConfigureLogging(logging => 
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
+                .Configure(app =>
+                {
+                    Console.WriteLine("Test");
+                    app.UseRouting();
+                    app.UseOcelot().Wait();                    
+                })
+                .Build()
+                .Run();
+        }
+    }
+}
